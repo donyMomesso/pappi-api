256b9759f6f6636eb8c1fce12bb72ff4ad3ecce5
donyMomesso <donyconfcargo@gmail.com>
2026-02-15 11:12:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(`🔥 Pappi API rodando na porta ${PORT}`);
+  console.log("🔥 Pappi API rodando na porta", PORT);
 });

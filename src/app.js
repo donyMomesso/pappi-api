46be876305c0adb665ff0bb1ca46fd709dbe5ea7
donyMomesso <donyconfcargo@gmail.com>
2026-02-15 12:29:54 -0300

    Add webhook routes to the application

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const publicRoutes = require("./routes/public.routes");
 const internalRoutes = require("./routes/internal.routes");
 const mapsRoutes = require("./routes/maps.routes");
+const webhookRoutes = require("./routes/webhook.routes");
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 
+app.use("/", webhookRoutes);
 app.use("/", publicRoutes);
 app.use("/", internalRoutes);
 app.use("/", mapsRoutes);

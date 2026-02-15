8909e4a7ca94d65b01cae694410e9a320afdc3aa
donyMomesso <donyconfcargo@gmail.com>
2026-02-15 02:57:48 -0300

    Refactor app initialization in index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const { createApp } = require("./src/app");
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 10000;
 
-const app = createApp();
-
 app.listen(PORT, () => {
-  console.log("🔥 Pappi API rodando na porta", PORT);
+  console.log(`🔥 Pappi API rodando na porta ${PORT}`);
 });

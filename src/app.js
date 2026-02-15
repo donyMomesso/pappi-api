0a75b3c429730f68c51915675b24b1e21a5b29e4
donyMomesso <donyconfcargo@gmail.com>
2026-02-15 11:28:36 -0300

    Update routing to include internal and maps routes
    Replaced protected routes with internal and maps routes.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 
 const publicRoutes = require("./routes/public.routes");
-const protectedRoutes = require("./routes/protected.routes");
+const internalRoutes = require("./routes/internal.routes");
+const mapsRoutes = require("./routes/maps.routes");
 
 const app = express();
+
 app.use(express.json({ limit: "10mb" }));
 
 // Rotas públicas
 app.use("/", publicRoutes);
 
-// Rotas protegidas
-app.use("/", protectedRoutes);
+// Rotas internas (protegidas por API KEY dentro do arquivo)
+app.use("/", internalRoutes);
+
+// Rotas de Maps
+app.use("/", mapsRoutes);
 
 // 404 padrão
-app.use((req, res) => res.status(404).json({ error: "not_found" }));
+app.use((req, res) => {
+  res.status(404).json({ error: "not_found" });
+});
 
 module.exports = app;

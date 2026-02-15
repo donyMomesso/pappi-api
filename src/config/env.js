f875d6305bed9af53db38a36cc4406d386a9919d
donyMomesso <donyconfcargo@gmail.com>
2026-02-15 02:43:48 -0300

    Add environment configuration using dotenv

--- src/config/env.js
+++ src/config/env.js
@@ -1,1 +1,12 @@
+require("dotenv").config();
+
+const ENV = {
+  PORT: process.env.PORT || 10000,
+  ATTENDANT_API_KEY: process.env.ATTENDANT_API_KEY || "",
+  GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY || "",
+  STORE_LAT: Number(process.env.STORE_LAT),
+  STORE_LNG: Number(process.env.STORE_LNG),
+};
+
+module.exports = ENV;
 

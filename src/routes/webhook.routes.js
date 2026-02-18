1e79d430b99cfc76992cf57c218e234d3321c75c
donyMomesso <donyconfcargo@gmail.com>
2026-02-18 14:45:12 -0300

    Update webhook.routes.js

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -14,20 +14,4 @@ router.get("/webhook", (req, res) => {
   return res.sendStatus(403);
 });
 
-module.exports = router;const express = require("express");
-const ENV = require("../config/env");
-const router = express.Router();
-
-// Apenas a verificação do Facebook (GET)
-router.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === ENV.WEBHOOK_VERIFY_TOKEN) {
-    return res.status(200).send(challenge);
-  }
-  return res.sendStatus(403);
-});
-
 module.exports = router;
